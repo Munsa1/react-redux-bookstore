ba2b29727bc1d4c08061752e09b10c4905b0d229
Munsa1 <81932478+Munsa1@users.noreply.github.com>
2021-12-15 12:45:51 +0200

    Add import for deleteBook from redux

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
-import { removeBook } from '../redux/books/books';
+import { deleteBook } from '../redux/Books/books';
 
 const BookItem = ({ book }) => {
   const {
