71dea48a82e5b504b415c200eb9098fe1f4f2a43
Munsa1 <81932478+Munsa1@users.noreply.github.com>
2021-12-16 15:28:55 +0200

    Add styling to navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,13 +3,24 @@ import { Link } from 'react-router-dom';
 import { FaUserAlt } from 'react-icons/fa';
 
 const Navbar = () => (
+  <div className="navigation_container">
   <nav className="navigation">
     <h1>Bookstore CMS</h1>
     <ul>
       <li><Link to="/">BOOKS</Link></li>
       <li><Link to="/categories">CATEGORIES</Link></li>
     </ul>
+    <span className="user-profile-wrapper">
+    <FaUserAlt style={{
+      color: '#0290ff',
+      height: '15px',
+      cursor: 'pointer',
+      transition: 'all ease-in 300ms',
+    }}
+    />
+  </span>
   </nav>
+  </div>
 );
 
 export default Navbar;
